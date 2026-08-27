f87e248a87817578e306adf97923234f0b762776
Mahmoud Ramadan Moustafa <mr3088289@gmail.com>
2026-08-27 22:11:02 +0300

    feat:add delete get

--- Routes/jobRoute.js
+++ Routes/jobRoute.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createJob, getAllJobs } from "../Controllers/jobController.js";
+import { createJob, getAllJobs, getjobbyid,updatejob, deletejob } from "../Controllers/jobController.js";
 import { authenticateUser , roleMiddleware} from "../Middlewares/auth.js";
 const jobRouter = Router();
 
 jobRouter.post("/", authenticateUser, roleMiddleware("employer", "admin"),createJob);
 jobRouter.get("/",getAllJobs);
-
+jobRouter.get("/",  getjobbyid);
+jobRouter.put("/",updatejob );
+jobRouter.delete("/",deletejob);
 
 export default jobRouter;
