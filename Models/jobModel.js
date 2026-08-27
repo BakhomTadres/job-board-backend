30b69dbf7ebd121ed4f00c85625ddbe0d48ac60d
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-08-27 21:42:00 +0300

    Add Files

---
+++ Models/jobModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema({
+    title :{
+        type : String,
+        required :true
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    description:{
+        type : String,
+        required :true
+    },
+    location:{
+        type : String,
+        required :true
+    },
+    salary:{
+        type :Number ,
+    },
+    type:{
+        type: String,
+        enum:['Full-time', 'Part-time', 'Remote', 'Freelance'],
+        required: true
+    },
+    employer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+export default mongoose.model('Job', jobSchema);
---
+++ Routes/applicationRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+        applyForJob,
+    getMyApplications,
+    getJobApplications,
+    updateApplicationStatus
+} 
+from "../Controllers/applicationController.js";
+
+const router = express.Router();
+
+router.post("/jobs/:id/apply",applyForJob);
+
+router.get("/applications/my",getMyApplications);
+
+router.get("/jobs/:id/applications",getJobApplications);
+
+router.patch("/applications/:id",updateApplicationStatus);
+
+export default router;
