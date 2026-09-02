7a363ac2118dd82db575449c6d24439814409638
Belal Ahmed <bkwjk0754@gmail.com>
2026-09-02 20:20:42 +0300

    fix home

--- Routes/jobRoute.js
+++ Routes/jobRoute.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { createJob, getAllJobs, getJobById, updateJob, deleteJob ,getRecommendedJobs } from "../Controllers/jobController.js";
+import { createJob, getAllJobs, getJobById, updateJob, deleteJob ,getRecommendedJobs, getJobStats } from "../Controllers/jobController.js";
 import { authenticateUser, roleMiddleware } from "../Middlewares/auth.js";
 
 const jobRouter = Router();
 
 jobRouter.post("/", authenticateUser, roleMiddleware("employer", "admin"), createJob);
 jobRouter.get("/", getAllJobs);
-jobRouter.get('/recommended', authenticateUser, getRecommendedJobs)
+jobRouter.get('/recommended', authenticateUser, getRecommendedJobs);
+jobRouter.get('/stats', getJobStats)
 jobRouter.get("/:id", getJobById);
 jobRouter.put("/:id", authenticateUser, roleMiddleware("employer", "admin"), updateJob);
 jobRouter.delete("/:id", authenticateUser, roleMiddleware("employer", "admin"), deleteJob);
